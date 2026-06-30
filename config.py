72d665c3f1beb095e2001013bcffe027ff9209c4
stepkovichayu <stepkovich.iek@gmail.com>
2026-06-30 11:57:06 +0300

    MIN_ORDER_USD = 20.5 MAX_ORDER_USD = 25

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GRID_CANCEL_SHIFT_PERCENT = 0.07
 # With DCA volume, later orders will be larger: level i notional = base * multiplier^(i-1).
 # The bot calculates base_qty so that total notional across all levels fits
 # the allocated budget (balance * usage% / num_symbols / 2 sides).
-MIN_ORDER_USD = 5.2
-MAX_ORDER_USD = 10
+MIN_ORDER_USD = 20.5
+MAX_ORDER_USD = 25
 
 # Percentage of available USDT balance to use for trading.
 BALANCE_USAGE_PERCENT = 50
